5735e3c8185e106347a22053e591eef9a977c482
Dilberovicka31 <dilberovicm36@gmail.com>
2020-11-27 15:41:58 -0800

    changing the portfolio look

---
+++ Assets/script.js
@@ -0,0 +1,14 @@
+let workEl = $("content2");
+let cardEl = (".card");
+let homeEl = $("#home");
+let contactEl = $("#contact-info")
+
+
+$("#home").on("click", function(){
+
+});
+
+$("#work").on("click", function(){
+workEl.style.display = "block";
+cardEl.style.display = "block";
+});
