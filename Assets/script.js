21577c70a11693592986aa5ce3a78a761fcdbcdb
Dilberovicka31 <dilberovicm36@gmail.com>
2020-11-28 21:50:02 -0800

    fixed js

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,29 @@
-$(document).ready(function() {
-$("#projects").hide();
-$("#contact-info").hide();
-  });
-  
-  $('#work').click(function() {
-    $('#projects').show(1000);
-    $('#contact-info').hide();
-    $('#first').hide(1000);
-    // $('#aboutMe').hide();
-  });
-  
-  $('#contact').click(function() {
-    $('#projects').hide(1000);
-    $('#contact-info').show(1000);
-    $('#first').hide();
-  });
-  
-  $('#home').click(function() {
-    $('#projects').hide();
-    $('#contact-info').hide();
-    $('#first').show(1000);
-  });
+$(document).ready(function () {
+  $("#project1").hide();
+  $("#project2").hide();
+  $("#project3").hide();
+  $("#contact-info").hide();
+});
+
+$("#work").click(function () {
+  $("#project1").show(1000);
+  $("#project2").show(1000);
+  $("#project3").show(1000);
+  $("#contact-info").hide();
+  $("#first").hide(1000);
+  // $('#aboutMe').hide();
+});
+
+$("#contact").click(function () {
+  $("#project1").hide(1000);
+  $("#project2").hide(1000);
+  $("#project3").hide(1000);
+  $("#contact-info").show(1000);
+  $("#first").hide();
+});
+
+$("#home").click(function () {
+  $("#projectsAll").hide();
+  $("#contact-info").hide();
+  $("#first").show(1000);
+});
