1b427e71f7ae841f3ece798ff2648438e95053a2
Dilberovicka31 <dilberovicm36@gmail.com>
2020-11-28 21:30:51 -0800

    added js and bootstrap classes

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,23 @@
-let workEl = $("content2");
-let cardEl = (".card");
-let homeEl = $("#home");
-let contactEl = $("#contact-info")
-
-
-$("#home").on("click", function(){
-
-});
-
-$("#work").on("click", function(){
-workEl.style.display = "block";
-cardEl.style.display = "block";
-});
+$(document).ready(function() {
+$("#projects").hide();
+$("#contact-info").hide();
+  });
+  
+  $('#work').click(function() {
+    $('#projects').show(1000);
+    $('#contact-info').hide();
+    $('#first').hide(1000);
+    // $('#aboutMe').hide();
+  });
+  
+  $('#contact').click(function() {
+    $('#projects').hide(1000);
+    $('#contact-info').show(1000);
+    $('#first').hide();
+  });
+  
+  $('#home').click(function() {
+    $('#projects').hide();
+    $('#contact-info').hide();
+    $('#first').show(1000);
+  });
