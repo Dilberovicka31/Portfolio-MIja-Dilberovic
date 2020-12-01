7859a096cdbbc1ae95e7527e32d2d813b55d6886
Dilberovicka31 <dilberovicm36@gmail.com>
2020-11-30 18:34:55 -0800

    html added a work section

--- Assets/script.js
+++ Assets/script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   $("#project1").hide();
   $("#project2").hide();
   $("#project3").hide();
+  $("#project4").hide();
   $("#contact-info").hide();
 });
 
@@ -9,6 +10,7 @@ $("#work").click(function () {
   $("#project1").show(1000);
   $("#project2").show(1000);
   $("#project3").show(1000);
+  $("#project4").show(1000);
   $("#contact-info").hide();
   $("#first").hide(1000);
   // $('#aboutMe').hide();
@@ -18,6 +20,7 @@ $("#contact").click(function () {
   $("#project1").hide(1000);
   $("#project2").hide(1000);
   $("#project3").hide(1000);
+  $("#project4").hide(1000);
   $("#contact-info").show(1000);
   $("#first").hide();
 });
@@ -26,6 +29,7 @@ $("#home").click(function () {
   $("#project1").hide();
   $("#project2").hide();
   $("#project3").hide();
+  $("#project4").hide();
   $("#contact-info").hide(1000);
   $("#first").show(1000);
 });
